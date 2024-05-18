d35c28f75950897d9f108e9b28c05bf415bb8b6e
traci13253 <127815154+traci13253@users.noreply.github.com>
2024-05-17 20:40:22 -0500

    Add files via upload

---
+++ file-vkj.file.txt
@@ -0,0 +1,1 @@
+git clone https://github.com/your-username/your-repository.git
---
+++ file-wsy.py
@@ -0,0 +1,5 @@
+# Install PyInstaller
+!pip install pyinstaller
+
+# Package the application
+!pyinstaller --onefile main.py
